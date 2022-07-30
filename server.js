c2d33fdadcab203e84363a1ebceb08c1483d1658
Daniel-trece <androvidd380@gmail.com>
2022-07-30 18:02:54 -0500

    Actualizacion

--- server.js
+++ server.js
@@ -10,4 +10,6 @@ app.get('/', (req,res)=>{
     res.render('index')
 })
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => 
+    console.log(`Servidor Express en el puerto: ${port}!`)
+)
