ca754d0fe3dee8b5cfb0cc399bc915c97b30478f
Daniel-trece <androvidd380@gmail.com>
2022-07-31 16:08:33 -0500

    Actualizacion responsive

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
+// DEPENDENCIAS
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000;
 
+// SERVIDOR LEVANTADO
+app.listen(port, () => 
+console.log(`Servidor Express en el puerto: ${port}!`)
+)
 
 app.use(express.static('public'))
+//DECLARACIÓN DE MOTOR DE PLANTILLAS EJS
 app.set('view engine', 'ejs')
-
+//DECLARACIÓN DE RUTA Y VISTA
 app.get('/', (req,res)=>{
     res.render('index')
 })
-
-app.listen(port, () => 
-    console.log(`Servidor Express en el puerto: ${port}!`)
-)
